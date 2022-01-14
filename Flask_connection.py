32db534833b2f729237e2b6449d9bfe9f9b6f466
danedebugs <73204753+danedebugs@users.noreply.github.com>
2022-01-14 10:50:40 -0800

    Update Flask_connection.py

--- Flask_connection.py
+++ Flask_connection.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='static')
 # configre db
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Lanamodern#1999'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'gatedown'
 
 
